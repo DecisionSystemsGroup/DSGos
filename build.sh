49757929236a15e003d2f07b9705f740ddfcb8ab
Konstantinos Gkotzamanidis <konstantinosgkotzamanidis@outlook.com>
2015-11-17 20:39:46 +0200

    Add i868 & x86_64

--- build.sh
+++ build.sh
@@ -236,17 +236,27 @@ done
 
 mkdir -p ${work_dir}
 
+cp -v releng/pacmanx86_64.conf releng/pacman.conf
 run_once make_pacman_conf
 
 # Do all stuff for each airootfs
-for arch in i686 x86_64; do
+for arch in x86_64; do
     run_once make_basefs
     run_once make_packages
+    run_once make_packages_efi
+    run_once make_setup_mkinitcpio
+    run_once make_customize_airootfs
 done
 
-run_once make_packages_efi
+echo make_pacman_conf i686
+cp -v releng/pacmani686.conf releng/pacman.conf
+cp -v releng/pacmani686.conf ${work_dir}/pacman.conf
 
-for arch in i686 x86_64; do
+
+for arch in i686; do
+    run_once make_basefs
+    run_once make_packages
+    run_once make_packages_efi
     run_once make_setup_mkinitcpio
     run_once make_customize_airootfs
 done
